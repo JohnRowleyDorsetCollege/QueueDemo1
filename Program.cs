8fe947fb0ad06c29c8e19087b12b9b953d4c4942
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-01-29 09:54:58 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Queue Demo");
+Queue<int> callerIds = new Queue<int>();
+
+callerIds.Enqueue(1);
+callerIds.Enqueue(2);
+callerIds.Enqueue(3);
+callerIds.Enqueue(4);
+
+Console.WriteLine($"Q has {callerIds.Count} items");
+
+if (callerIds.Count > 0)
+{
+    Console.WriteLine($"First item is {callerIds.Peek()}");
+}
+
+foreach (var id in callerIds)
+{
+    Console.Write(id);
+}
+
+
+while (callerIds.Count > 0)
+{
+    var queueItem = callerIds.Dequeue();
+    Console.WriteLine($"Processing item [{queueItem}]");
+    if (callerIds.Contains(3))
+    {
+        Console.WriteLine($"The id 3 is still in the Q");
+    }
+}
+
+Console.WriteLine($"Processed Q has {callerIds.Count} items");
+
+// should throw exception
+try
+{
+    // throw new Exception("Boom");
+    callerIds.Dequeue();
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine($"Warning: There is nothing left in the Q to process");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Warning: Unknown Exception {e.Message}");
+}
